bfb6d29709b08dcd01551172aa9c8060843e632e
Geraldo Castro <victormatheuscastro@gmail.com>
2019-03-06 20:26:33 -0300

    Add initial project setup

---
+++ main.py
@@ -0,0 +1,37 @@
+from loguru import logger
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import config
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Hi!')
+
+
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Help!')
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text, reply_to_message_id=update.message.message_id)
+
+
+def error(bot, update):
+    logger.warning(f'Update {bot} caused error {update.error}')
+
+
+def main():
+    updater = Updater(config.BOT_TOKEN)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
